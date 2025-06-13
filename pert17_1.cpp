96af805e076c6cc9b74ff9c3843531ad9a42b8e5
135313 <ikhwanhanif135@gmail.com>
2025-06-13 19:09:35 +0700

    Add files via upload

---
+++ pert17_1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+// Fungsi untuk menampilkan garis
+void garis()
+{
+    cout << "----------------------" << endl;
+}
+
+int main()
+{
+    garis();
+    cout << "AMIK BSI - Pondok Labu" << endl;
+    garis();
+
+    return 0;
+}
+
